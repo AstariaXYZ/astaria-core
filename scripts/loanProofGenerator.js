c73ac84e4254e7022ed2964b5e95b3c21cbbee10
Andrew Redden <andrewredden@Andrews-MBP.localdomain>
2022-07-25 15:07:38 -0300

    loan proof generator updated wip

--- scripts/loanProofGenerator.js
+++ scripts/loanProofGenerator.js
@@ -22,41 +22,73 @@ const keccak256 = solidityKeccak256;
 // address, tokenId, maxAmount, maxDebt, interest, maxInterest, duration, schedule
 const leaves = [];
 
-// const loan = keccak256(
-//   ["uint256", "uint256", "uint256", "uint256", "uint256"],
-//   [
-//     BigNumber.from(args[2]),
-//     BigNumber.from(args[3]),
-//     BigNumber.from(args[4]),
-//     BigNumber.from(args[5]),
-//     BigNumber.from(args[6]),
-//   ]
-// );
 const tokenAddress = args[0];
 const tokenId = BigNumber.from(args[1]).toString();
 const collateral = keccak256(["address", "uint256"], [tokenAddress, tokenId]);
-const loan = [
-  BigNumber.from(args[2]), // maxAmount
-  BigNumber.from(args[3]), // maxDebt
-  BigNumber.from(args[4]), // rate
-  BigNumber.from(args[5]), // maxRate
-  BigNumber.from(args[6]), // duration
-  BigNumber.from(args[7]), // schedule
-];
-leaves.push(
-  keccak256(
-    [
-      "bytes32", // bytes32 casting of uint256
-      "uint256", // maxAmount
-      "uint256", // maxDebt
-      "uint256", // rate
-      "uint256", // maxRate
-      "uint256", // duration
-      "uint256", // schedule
-    ],
-    [collateral, ...loan]
-  )
+
+const strategyDetails = keccak256(
+  [
+    "uint8",
+    "uint8",
+    "address",
+    "address",
+    "bool",
+    "uint256",
+    "uint256",
+    "address",
+  ],
+  [
+    BigNumber.from(0), // type
+    BigNumber.from(0), // version
+    getAddress(args[2]), // strategist
+    getAddress(args[3]), // delegate
+    BigNumber.from(args[4]), // public
+    BigNumber.from(args[5]), // expiration
+    BigNumber.from(args[6]), // nonce
+    getAddress(args[7]), // vault
+  ]
 );
+
+const detailsType = args[8];
+let details;
+if (detailsType === 1) {
+  details = keccak256(
+    ["uint8", "address", "uint256", "address", "bytes"],
+    [
+      BigNumber.from(1), // type
+      getAddress(args[9]), // token
+      BigNumber.from(args[10]), // tokenId
+      getAddress(args[11]), // borrower
+      solidityPack(["bytes"], [args[12]]), // lien
+    ]
+  );
+} else if (detailsType === 2) {
+  details = keccak256(
+    ["uint8", "address", "address", "bytes"],
+    [
+      BigNumber.from(2), // type
+      getAddress(args[9]), // token
+      getAddress(args[10]), // borrower
+      solidityPack(["bytes"], [args[11]]), // lien
+    ]
+  );
+}
+
+// struct Terms {
+//     strategyType: uint8;
+//     strategyVersion: uint8;
+//     expiration: uint256;
+//     nonce: uint256;
+//     vault: address;
+//     strategy: address;
+//
+//     loanType: uint8;
+//     loanData: bytes;
+// }
+
+leaves.push(strategyDetails);
+
+leaves.push(keccak256([collateral, details]));
 // Create tree
 const merkleTree = new MerkleTree(leaves, keccak256, { sort: true });
 // Get root
