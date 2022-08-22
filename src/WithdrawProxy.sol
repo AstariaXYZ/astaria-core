6c5b4822059dc8e06627688f6e58070c256769cd
Andrew Redden <andrewredden@Andrews-MacBook-Pro.local>
2022-08-23 01:28:34 +0200

    remove some commented out imports from withdraw proxy

--- src/WithdrawProxy.sol
+++ src/WithdrawProxy.sol
@@ -1,9 +1,6 @@
 pragma solidity ^0.8.16;
 
 import {Auth, Authority} from "solmate/auth/Auth.sol";
-// import {SafeTransferLib, ERC20} from "solmate/utils/SafeTransferLib.sol";
-// import {ERC20} from "solmate/tokens/ERC20.sol";
-// import {ERC20} from "openzeppelin/token/ERC20/ERC20.sol";
 import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
 import {ERC20Cloned, IBase} from "gpl/ERC4626-Cloned.sol";
 import {ERC20} from "solmate/tokens/ERC20.sol";
@@ -11,12 +8,26 @@ import {IERC20} from "openzeppelin/token/ERC20/IERC20.sol";
 import {ITransferProxy} from "gpl/interfaces/ITransferProxy.sol";
 
 contract WithdrawProxy is ERC20Cloned {
-    function name() public view override (IBase) returns (string memory) {
-        return string(abi.encodePacked("AST-WithdrawVault-", ERC20(underlying()).symbol()));
+    function name() public view override(IBase) returns (string memory) {
+        return
+            string(
+                abi.encodePacked(
+                    "AST-WithdrawVault-",
+                    ERC20(underlying()).symbol()
+                )
+            );
     }
 
-    function symbol() public view override (IBase) returns (string memory) {
-        return string(abi.encodePacked("AST-W", owner(), "-", ERC20(underlying()).symbol()));
+    function symbol() public view override(IBase) returns (string memory) {
+        return
+            string(
+                abi.encodePacked(
+                    "AST-W",
+                    owner(),
+                    "-",
+                    ERC20(underlying()).symbol()
+                )
+            );
     }
 
     using SafeTransferLib for ERC20;
@@ -25,11 +36,17 @@ contract WithdrawProxy is ERC20Cloned {
         require(balanceOf[msg.sender] >= amount, "insufficient balance");
         _burn(msg.sender, amount);
         ERC20(underlying()).safeTransfer(
-            msg.sender, (amount / totalSupply) * ERC20(underlying()).balanceOf(address(this))
+            msg.sender,
+            (amount / totalSupply) *
+                ERC20(underlying()).balanceOf(address(this))
         );
     }
 
-    function mint(address receiver, uint256 shares) public virtual returns (uint256 assets) {
+    function mint(address receiver, uint256 shares)
+        public
+        virtual
+        returns (uint256 assets)
+    {
         require(msg.sender == owner(), "only owner can mint");
         _mint(receiver, shares);
     }
