1e8b58b549ed71612be3fb3adb80b3aa5511473e
Andrew Redden <=>
2022-10-21 12:03:55 -0300

    updates for the deploy flow and file/fileBatch on Router

---
+++ src/scripts/deployments/AstariaStack.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8.0;
+
+import "forge-std/Script.sol";
+
+//goerli deployments
+contract AstariaStack is Script {
+  address WETH9_ADDR = vm.envAddress("WETH9_ADDR");
+  address ROUTER_ADDR = vm.envAddress("ROUTER_ADDR");
+  address MRA_ADDR = vm.envAddress("MRA_ADDR");
+  address TRANSFER_PROXY_ADDR = vm.envAddress("TRANSFER_PROXY_ADDR");
+  address LIEN_TOKEN_ADDR = vm.envAddress("LIEN_TOKEN_ADDR");
+  address COLLATERAL_TOKEN_ADDR = vm.envAddress("COLLATERAL_TOKEN_ADDR");
+  address SOLO_IMPL_ADDR = vm.envAddress("SOLO_IMPL_ADDR");
+  address PUBLIC_IMPL_ADDR = vm.envAddress("PUBLIC_IMPL_ADDR");
+  address WITHDRAW_PROXY_ADDR = vm.envAddress("WITHDRAW_PROXY_ADDR");
+  address LIQUIDATION_ACCOUNTANT_ADDR =
+    vm.envAddress("LIQUIDATION_ACCOUNTANT_ADDR");
+  address AUCTION_HOUSE_ADDR = vm.envAddress("AUCTION_HOUSE_ADDR");
+}
