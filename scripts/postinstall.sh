99508e4d3b2dcae9af449f3502d524ca50bbec47
Andrew Redden <=>
2022-11-10 14:05:49 -0400

    build to dist instead of individual

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -3,9 +3,9 @@ NPX="npx"
 
 cd lib/astaria-sdk && yarn && yarn build && cd ../..
 
-if [[ -z $CI ]]; then
+if [[ -z $CI ]] ; then
   NPX=
 fi
-SCRIPT="${NPX-:""} tsc scripts/loanProofGenerator.ts"
+SCRIPT="${NPX-:""} tsc"
 
 ${SCRIPT}
