393aac59fc946bb17b8d50680851ef0dc42bf20c
Andrew Redden <=>
2022-12-06 06:46:48 -0400

    make the core contracts upgradeable

--- src/LienToken.sol
+++ src/LienToken.sol
@@ -38,7 +38,7 @@ import {Initializable} from "./utils/Initializable.sol";
  * @title LienToken
  * @notice This contract handles the creation, payments, buyouts, and liquidations of tokenized NFT-collateralized debt (liens). Vaults which originate loans against supported collateral are issued a LienToken representing the right to loan repayments and auctioned funds on liquidation.
  */
-contract LienToken is ERC721, ILienToken, AuthInitializable {
+contract LienToken is ERC721, ILienToken, AuthInitializable, Initializable {
   using FixedPointMathLib for uint256;
   using CollateralLookup for address;
   using SafeCastLib for uint256;
@@ -436,16 +436,11 @@ contract LienToken is ERC721, ILienToken, AuthInitializable {
       if (params.lien.collateralId != params.stack[0].lien.collateralId) {
         revert InvalidState(InvalidStates.COLLATERAL_MISMATCH);
       }
-
-
-      if (params.lien.token != params.stack[0].lien.token) {
-        revert InvalidState(InvalidStates.ASSET_MISMATCH);
-      }
     }
 
-      if (params.lien.token != params.stack[0].lien.token) {
-        revert InvalidState(InvalidStates.ASSET_MISMATCH);
-      }
+
+    if (params.lien.token != params.stack[0].lien.token) {
+      revert InvalidState(InvalidStates.ASSET_MISMATCH);
     }
 
     newLienId = uint256(keccak256(abi.encode(params.lien)));
