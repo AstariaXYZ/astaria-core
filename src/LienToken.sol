47ee6972dd103c0642499c4ce0c9cd3797bb117d
Santiago Lisa <santiagogregoryl@gmail.com>
2022-12-13 06:38:58 -0500

    fixed visibility (#192)

--- src/LienToken.sol
+++ src/LienToken.sol
@@ -662,7 +662,7 @@ contract LienToken is ERC721, ILienToken, Auth {
   }
 
   function _getMaxPotentialDebtForCollateral(Stack[] memory stack)
-    public
+    internal
     pure
     returns (uint256 maxPotentialDebt)
   {
