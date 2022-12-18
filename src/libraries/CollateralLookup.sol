8cccd90d3f59c08e73bc4f1023ba1cb0cfc4bb75
= <santiagogregoryl@gmail.com>
2022-12-18 16:06:42 -0500

    used scratch space for CollateralLookup hash, https://github.com/spearbit-audits/astaria/issues/31

--- src/libraries/CollateralLookup.sol
+++ src/libraries/CollateralLookup.sol
@@ -16,8 +16,12 @@ library CollateralLookup {
   function computeId(address token, uint256 tokenId)
     internal
     pure
-    returns (uint256)
+    returns (uint256 hash)
   {
-    return uint256(keccak256(abi.encodePacked(token, tokenId)));
+    assembly {
+      mstore(0, token) // sets the right most 20 bytes in the first memory slot.
+      mstore(0x20, tokenId) // stores tokenId in the second memory slot.
+      hash := keccak256(12, 52) // keccak from the 12th byte up to the entire second memory slot.
+    }
   }
 }
