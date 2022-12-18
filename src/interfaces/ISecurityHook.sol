aa465c24c6cceb2b42e2151374262503c1635974
= <santiagogregoryl@gmail.com>
2022-12-18 14:22:55 -0500

    updated ISecurityHook.getState() to return hash, https://github.com/spearbit-audits/astaria/issues/102

--- src/interfaces/ISecurityHook.sol
+++ src/interfaces/ISecurityHook.sol
@@ -11,5 +11,5 @@
 pragma solidity =0.8.17;
 
 interface ISecurityHook {
-  function getState(address, uint256) external view returns (bytes memory);
+  function getState(address, uint256) external view returns (bytes32);
 }
