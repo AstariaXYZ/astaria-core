f56f416bc4559bb58d6ab42561e79a7b3344c8b4
= <santiagogregoryl@gmail.com>
2022-12-18 16:04:56 -0500

    removed unnecessary mulDivDown, https://github.com/spearbit-audits/astaria/issues/35

--- src/LienToken.sol
+++ src/LienToken.sol
@@ -46,6 +46,7 @@ contract LienToken is ERC721, ILienToken, Auth {
     uint256(keccak256("xyz.astaria.LienToken.storage.location")) - 1;
 
   bytes32 constant ACTIVE_AUCTION = bytes32("ACTIVE_AUCTION");
+
   /**
    * @dev Setup transfer authority and initialize the buyoutNumerator and buyoutDenominator for the lien buyout premium.
    * @param _AUTHORITY The authority manager.
@@ -134,8 +135,7 @@ contract LienToken is ERC721, ILienToken, Auth {
     }
 
     if (
-      s.collateralStateHash[params.encumber.lien.collateralId] ==
-      ACTIVE_AUCTION
+      s.collateralStateHash[params.encumber.lien.collateralId] == ACTIVE_AUCTION
     ) {
       revert InvalidState(InvalidStates.COLLATERAL_AUCTION);
     }
@@ -255,7 +255,7 @@ contract LienToken is ERC721, ILienToken, Auth {
     uint256 delta_t = timestamp - stack.point.last;
 
     return
-      delta_t.mulDivDown(stack.lien.details.rate, 1).mulWadDown(
+      (delta_t.mulDivDown * stack.lien.details.rate).mulWadDown(
         stack.point.amount
       );
   }
@@ -409,9 +409,7 @@ contract LienToken is ERC721, ILienToken, Auth {
     LienStorage storage s,
     ILienToken.LienActionEncumber memory params
   ) internal returns (uint256 newLienId, ILienToken.Stack memory newSlot) {
-    if (
-      s.collateralStateHash[params.collateralId] == ACTIVE_AUCTION
-    ) {
+    if (s.collateralStateHash[params.collateralId] == ACTIVE_AUCTION) {
       revert InvalidState(InvalidStates.COLLATERAL_AUCTION);
     }
     if (
@@ -774,7 +772,7 @@ contract LienToken is ERC721, ILienToken, Auth {
   {
     uint256 delta_t = stack.point.end - block.timestamp;
     return
-      delta_t.mulDivDown(stack.lien.details.rate, 1).mulWadDown(
+      (delta_t.mulDivDown * stack.lien.details.rate).mulWadDown(
         stack.point.amount
       );
   }
