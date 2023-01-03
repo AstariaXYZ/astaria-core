6976e0ee98968a3cb7c1f614779f5f6d3d360679
Joseph Delong <joseph@delong.me>
2023-01-02 21:09:03 -0600

    remove comments from boot-system

--- scripts/boot-system.sh
+++ scripts/boot-system.sh
@@ -2,9 +2,3 @@
 source .env
 forge script Deploy --broadcast --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --rpc-url http://localhost:8545
 forge script UniqueStrategy --broadcast --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --rpc-url http://localhost:8545
-
-# we're on a fork so get some weth
-
-# create some vaults
-
-# create some
