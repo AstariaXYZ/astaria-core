df8818df8562a29b6f922f7531c6c3390a80ada9
Andrew Redden <=>
2023-05-02 10:42:09 -0300

    add stop prank

--- src/test/AstariaTest.t.sol
+++ src/test/AstariaTest.t.sol
@@ -659,6 +659,7 @@ contract AstariaTest is TestHelpers {
     vm.startPrank(strategistOne);
 
     Receiver(receiverCreated).withdraw(ERC20(address(token)), 10 ether);
+    vm.stopPrank();
   }
 
   function testEpochProcessionMultipleActors() public {
