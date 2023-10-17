2ca11924f5479458e47c877ce6f8c984699dae3f
Andrew Redden <=>
2023-10-17 10:30:04 -0300

    update the locker to apply more metadata to the tokenURI output

--- src/TheLocker.sol
+++ src/TheLocker.sol
@@ -3,7 +3,73 @@ pragma solidity =0.8.17;
 import "gpl/ERC721.sol";
 import {ERC20} from "solady/tokens/ERC20.sol";
 import {SafeTransferLib} from "solady/utils/SafeTransferLib.sol";
-import {LibString} from "solady/utils/LibString.sol";
+
+/**
+ * @dev String operations.
+ */
+library Strings {
+  bytes16 private constant _HEX_SYMBOLS = "0123456789abcdef";
+
+  /**
+   * @dev Converts a `uint256` to its ASCII `string` decimal representation.
+   */
+  function toString(uint256 value) internal pure returns (string memory) {
+    // Inspired by OraclizeAPI's implementation - MIT licence
+    // https://github.com/oraclize/ethereum-api/blob/b42146b063c7d6ee1358846c198246239e9360e8/oraclizeAPI_0.4.25.sol
+
+    if (value == 0) {
+      return "0";
+    }
+    uint256 temp = value;
+    uint256 digits;
+    while (temp != 0) {
+      digits++;
+      temp /= 10;
+    }
+    bytes memory buffer = new bytes(digits);
+    while (value != 0) {
+      digits -= 1;
+      buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
+      value /= 10;
+    }
+    return string(buffer);
+  }
+
+  /**
+   * @dev Converts a `uint256` to its ASCII `string` hexadecimal representation.
+   */
+  function toHexString(uint256 value) internal pure returns (string memory) {
+    if (value == 0) {
+      return "0x00";
+    }
+    uint256 temp = value;
+    uint256 length = 0;
+    while (temp != 0) {
+      length++;
+      temp >>= 8;
+    }
+    return toHexString(value, length);
+  }
+
+  /**
+   * @dev Converts a `uint256` to its ASCII `string` hexadecimal representation with fixed length.
+   */
+  function toHexString(
+    uint256 value,
+    uint256 length
+  ) internal pure returns (string memory) {
+    bytes memory buffer = new bytes(2 * length + 2);
+    buffer[0] = "0";
+    buffer[1] = "x";
+    for (uint256 i = 2 * length + 1; i > 1; --i) {
+      buffer[i] = _HEX_SYMBOLS[value & 0xf];
+      value >>= 4;
+    }
+    require(value == 0, "Strings: hex length insufficient");
+    return string(buffer);
+  }
+}
+
 import {IERC721} from "core/interfaces/IERC721.sol";
 
 interface ILocker is IERC721 {
@@ -93,10 +159,19 @@ contract TheLocker is ERC721, ILocker {
           "data:application/json,",
           "{",
           "'asset': '",
-          LibString.toString(uint256(uint160(address(deposit.token)))),
+          Strings.toHexString(uint256(uint160(address(deposit.token)))),
+          "',",
+          "'name': '",
+          ERC20(deposit.token).name(),
+          "',",
+          "'symbol': '",
+          ERC20(deposit.token).symbol(),
+          "',",
+          "'decimals': '",
+          Strings.toString(ERC20(deposit.token).decimals()),
           "',",
           "'amount': '",
-          LibString.toString(deposit.amount),
+          Strings.toString(deposit.amount),
           "'}"
         )
       );
